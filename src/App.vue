30c4a0906104950340505958bb1efda729b5f2ad
Vikash <vikashvickyssec@gmail.com>
2021-02-26 19:42:21 +0530

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <div id="app">
+    <h2>Making radio components</h2>
+    <custom-radio name="radio-1" />
+    <custom-radio name="radio-1" />
+  </div>
+</template>
+
+<script>
+import CustomRadio from "./components/CustomRadio";
+
+export default {
+  name: "App",
+  components: { CustomRadio },
+};
+</script>
+
+<style>
+#app {
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/CustomRadio.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="custom-radio">
+    <input type="radio" v-model="_value" :name="name" />
+    <label>
+      <slot>{{ name }}</slot>
+    </label>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "CustomRadio",
+  data() {
+    return {};
+  },
+  props: {
+    value: { default: false },
+    name: { type: String, default: "label" },
+  },
+  computed: {
+    _value: {
+      get() {
+        return this.value;
+      },
+      set(value) {
+        this.$emit("input", value);
+      },
+    },
+  },
+};
+</script>
+
+<style>
+</style>
